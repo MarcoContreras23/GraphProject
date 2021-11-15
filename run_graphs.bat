c43809764e1a5b8c2cc381eb9e2e091bef15a0e3
MarcoContreras23 <marcocontreras2309@gmail.com>
2021-11-15 18:59:44 -0500

    Results

--- run_graphs.bat
+++ run_graphs.bat
@@ -1,8 +1,8 @@
 SET PYPHI_WELCOME_OFF='yes'
 SET PYTHONIOENCODING='utf8'
 
-for %%f in (graphs/*) do (
+for %%f in (executionFiles/*) do (
   echo "----------" >> results_bat.txt
   echo %%f >> results_bat.txt
-  python graphs/%%f >> results_bat.txt
+  python executionFiles/%%f >> results_bat.txt
 )
